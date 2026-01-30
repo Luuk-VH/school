aa8b6c011917336b19c453f994f2fa5b468bb8d9
Luuk van Hage <s1154358@student.hsleiden.nl>
2026-01-30 13:17:39 +0100

    d

--- luuk/scripts/timeline.js
+++ luuk/scripts/timeline.js
@@ -1,30 +1,87 @@
 const blogData = {
-    1: { title: "WEEK 1: ANALYSE", text: "IN DEZE EERSTE WEEK HEB IK DE DESIGN THINKING METHODE TOEGEPAST OM MIJN DOELGROEP IN KAART TE BRENGEN.", media: "../assets/images/test.jpg" },
-    2: { title: "WEEK 1: DESIGN KEUZES", text: "IK HEB GEKOZEN VOOR EEN MODERN RETRO POKÉMON THEMA, WAARBIJ NAVIGATIE CENTRAAL STAAT.", media: "../assets/images/test.jpg" },
-    3: { title: "WEEK 1: PLANNING", text: "MIJN GANTT-CHART IS OPGEZET OM ZOWEL GROEPS- ALS INDIVIDUELE DEADLINES TE BEWAKEN.", media: "../assets/images/test.jpg" },
-    4: { title: "WEEK 2: HARDWARE", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
-            "Why do we use it?\n" +
-            "\n" +
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n" +
-            "\n" +
-            "Where does it come from?\n" +
-            "\n" +
-            "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n" +
-            "\n" +
-            "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n" +
-            "Where can I get some?\n" +
-            "\n" +
-            "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.", media: "../assets/images/test.jpg" }
+    1: {
+        title: "WEEK 1: INLEIDING",
+        text: "<strong>Inleiding:</strong> I.V.M. Persoonlijke omstandigheden was ik de eerste week van de module niet aanwezig. Ik heb hier ook niet aan school gewerkt dus ik kan hier over deze week ook niet veel vertellen.",
+        media: ""
+    },
+    2: {
+        title: "WEEK 2: BRAINSTORM & PVA",
+        text: "<strong>Inleiding:</strong> Na de eerste week aanwezig te zijn had ik deze week ook mijn groepje gevormd. Ik ben aangesloten bij 3 andere groepsleden (Mohammed, Ibrahim, Hamayoen).\nVanaf dit punt zijn wij begonnen met brainstormen voor welk thema wij zouden willen.\n\n<strong>Plan van Aanpak</strong>\nOm te besluiten hoe wij als groep willen verder werken en de eisen willen vast stellen hebben wij een PVA opgesteld.\nDit is nodig om verder door te kunnen gaan met de dingen die ons te doen staan zoals ontwerpen, afspraken nakomen en uiteindelijk een product op te kunnen leveren met de vereisten.\nDit hebben we binnen een meeting opgezet en heb ik deze week thuis verder opgemaakt en aangepast zodat het netjes verwoord staat.",
+        media: ""
+    },
+    3: {
+        title: "WEEK 2: PLANNING & BLOG",
+        text: "<strong>Planning</strong>\nNu de PVA (Plan van Aanpak) vastgesteld is kan er een planning gemaakt worden met wie wat moet doen.\nAlle doelen zijn in zicht dus een planning maken is wel ideaal, dit helpt ook ons om een overzicht te behouden zowel invidueel als groep wat ons te wachten staat en wat wij moeten doen.\nDeze planning is die week in een meeting ook vastgesteld.\n\n<strong>Webblog</strong>\nOmdat ik in de eerste week niet aanwezig kon zijn heb ik ook mijn blog opgezet, nog niet met een ontwerp maar in iedergeval dat ik de basis heb en iedere dag van de week mijn blog eventueel bij kan werken.",
+        media: ""
+    },
+    4: {
+        title: "WEEK 2: HARDWARE & WEBSITE",
+        text: "<strong>Hardware</strong>\nOmdat ik achter liep ben ik ook bezig geweest met de hardware van de PI. Ik heb deze week ook de PI box opgehaald bij Fred en ben ik thuis aan de slag gegaan met de werkboeken / het huiswerk van week 1.\nIk ben begonnen met het schakelaar werkboek, het begon vrijmakkelijk en ben toen ook over gegaan op het shutdown werkboek waarmee je je pi moest uitzetten met een knop waarvan een script aan het meeluisteren was die via een cronjob opgestart was.\nIk ben het huiswerk ook gaan maken, ik heb de huiswerk opdrachten Hardware Huiswerk 1.2 gemaakt (Morse Decoder & Data input). Dit liep voor mij nog vrij makkelijk af en begreep het ook direct door de gemaakte werkboeken.\n\n<strong>Website</strong>\nIk heb voor de website eigenlijk er niet aan gewerkt, ik had mijn focus deze week vooral gelegt op de stof die ik gemist had wat met hardware te maken had.\nIk heb wel de structuur opgezet en het blog zodat ik het blog op zijn minst bij kon houden.",
+        media: ""
+    },
+    5: {
+        title: "WEEK 3: PLANNING & EXCEL",
+        text: "<strong>Inleiding:</strong>\nVanaf de 3e week in het nieuwe jaar begon ik mijn groepje ook minder te zien, of ze waren niet aanwezig of ook ziek heb ook meerdere keren gevraagd van zijn jullie vandaag op school of wie is er vandaag aanwezig?\nDeze week ben ik meer begonnen met inhalen en een planning voor mij zelf te maken omdat ik verder achter liep omdat ik ziek was geworden.\n\n<strong>Planning.</strong>\nVanaf het begin van de nieuwe school week was ik ziek zoals ik vermeld had, hierdoor liep ik nog verder achter. Toen ik beter was ben ik begonnen met het maken van een PVA voor mijzelf voor het huiswerk wat ik moet doen.\nVanaf dan ben ik ook beetje zelf dingen gaan beslissen om verder te kunnen. Ik heb hier een hele overizhct gemaakt in EXCEL van al het huiswerk en werkboeken die gemaakt moesten worden om bij te komen met de stof.\nIk heb in excel hier een duidelijk overzicht voor mijzelf van gemaakt van wat ik gemaakt heb, wat nog gemaakt moest worden of waarmee ik bezig was.",
+        media: ""
+    },
+    6: {
+        title: "WEEK 3: HARDWARE & STYLE GUIDE",
+        text: "<strong>Hardware</strong>\nNa de 2e week ging de 3e na de kerst vakantie al weer waar ik direct het nieuwe jaar aftrapte met ziek zijn.\nIk heb in de vakantie begonnen met het maken van de Style Guide. Ik had deze taak op mij genomen op basis van de wensen die iedereen had na het opzetten van de Plan van Aanpak.\nIk heb hier ook grotendeels het achterstallig hardware huiswerk gemaat. Waar ik leerde werken met een Led & een Buzzer. Hier kwamen de eerste ideen al naar boven van mijn project doe ik opgeschreven heb in mijn schrift.\nIk ben ook begonnen met de rottery encoder wat even ingewikkeld was maar het lukte mij wel uiteindelijk. Ik liep vooral beetje vast op met het draaien dat hij halverwege de \"klik\" draai al switchte naar een ander nummer.\nIk ben verder gegaan met ook de RGB Led's wat ook leuk was om mee te werken met al die verschillende kleuren uit 1 led.",
+        media: ""
+    },
+    7: {
+        title: "WEEK 3: SCHUIFREGISTERS & AI",
+        text: "Ik ben aan het einde van de week toegekomen ook aan de 7 segment display en de schuifregister. Dit was even een puzzel maar doordat ik Gemini (AI) het werkboek geupload heb en gevraagd maak meer en duidelijkere oefen opdrachten voor mij met instructies die ik gebruiken om schuifregisters beter te leren toepassen. Dit werkte ideaal voor mij waardoor ik het beter begreep.\n\n<strong>Webblog:</strong>\nDoordat ik achterliep had ik nog geen aandacht besteed aan de andere onderwerpen.\nIk ben alleen mijn weekblog wezen updaten op basis van wat ik gedaan heb. Ik heb ook nog geen ontwerp gemaakt.",
+        media: ""
+    },
+    8: {
+        title: "WEEK 4: HARDWARE & TEAMDYNAMIEK",
+        text: "<strong>Inleiding:</strong> Hier kreeg ik al snel te horen dat het groepje al aangaf dat ze het onderdeel waarschijnlijk van de puzzel niet gaan doen. Ik had hier al een beetje een naar gevoel over hoe het verder uit zou pakken.\nIn iedergeval communiceerde ze dit wel wat wel fijn was. Ze gaven aan op de andere dingen te focussen zoals de websites etc. Ik ben mijzelf deze week ook meer gaan focussen op het front-end.\n\n<strong>Hardware:</strong>\nIk ben verder gegaan met al het hardware huiswerk. Ik heb van week 3 het huiswerk van les 2 gemaakt. Hier moesten we werken met een Bar Display en een multi segment display. Ik heb hier geleerd om een treintje te maken met de schuifregisters zodat ik met maar 3 gpio pinnen 2 display componenten kon aansturen. Dit gaf mij ook ideeen voor mijn puzzel waar ik eigenlijk nog niet aan begonnen was door alle omstandigheden. Ik heb toen ook gewerkt aan het matrix display wat ik lastig maar erg leuk was. Ik heb hiermee thuis vooral mee lopen te puzzelen.",
+        media: ""
+    },
+    9: {
+        title: "WEEK 4: ONTWERP & FEEDBACK",
+        text: "<strong>Ontwerp:</strong>\nIk heb de wireframes gemaakt van de groepswebsite & mijn inviduele site die nog uitgewerkt moest worden.\nDeze heb ik dus ook van mijn invduele site ingeleverd in FeedbackFruits. Ik heb hier ook feedback gegeven op anderen maar ik heb ook hun feedback verwerkt in mijn site!\n<strong>FEEDBACK VOORBEELDEN</strong>",
+        media: ""
+    },
+    10: {
+        title: "WEEK 4: GROEPS- & PERSOONLIJKE SITE",
+        text: "<strong>Groepswebsite:</strong>\nIk ben hier daadwerkelijk begonnen met het maken van de groepswebsite op basis van de wireframe & styleguide die wij gemaakt hebben en de eisen die we opgesteld hebben.\nHiervan heb ik aangegeven dat ik de groepswebsite wel zou maken zodat hun op persoonlijke site konden richten ( uit de grond van mijn hart :) ). Ik heb hier de groepswebsite gemaakt.\nIk heb gekeken naar verschillende punten die verplicht waren zoals: Semantisch correcte HTML, errorloze css en errorloze javascript. Ik heb deze gemaakt op basis van de groep styleguide die gemaakt was.\n\n<strong>Persoonlijke website:</strong>\nIk ben ook begonnen met het ontwerpen van mijn persoonlijke website. De home pagina was vrijsnel klaar dat was niet zo heel moeilijk om af te ronden omdat ik niet veel componenten nodig had.\nIk was hier nog lang niet mee klaar omdat de tijdlijn op de blog pagina een lastig dingetje was om het responsive te maken.",
+        media: ""
+    },
+    11: {
+        title: "WEEK 5: PUZZEL & DOCUMENTATIE",
+        text: "<strong>Inleiding:</strong>\nDe laatste week voordat de toetsen waren, hier kreeg ik echt volledig definitief te horen dat niemand van mijn groepje op mij na ging voor de toetsen en dat ze voor de herkansingen gingen.\nDit was ff zwaar en lastig omdat ik nu van alles in 1 keer moest doen en dat sommige documenten die er hoorde te zijn er niet waren of niet uitgewerkt waren.\n\n<strong>Puzzel maken:</strong>\nIk ben gaan brainstormen over de dingen waarmee ik gewerkt heb met het huiswerk maken. Toen leek mij het leuke idee om 2/3 spellen in 1 te doen waar je 3 levens bij nodig hebt. Dit maakt het uitdagend voor zowel mij als de gebruiker maar ook leuk van hoe ik het zou willen realiseren. Ik heb in mijn schrift getekent hoe mijn schoenen doos verdeeld kon zijn en welke componenten ik nodig zou hebben.\nToen ik de puzzel ging maken heb ik een begin opgesteld met de multi segment display en de rotery encoder. Ik had die meegenomen naar school en eenmaal toen ik weer thuis was waren de draadjes bij de schuifregisters eruit en dat was niet leuk. Ik moest toen weer uitzoeke n hoe ik het gedaan heb en heb toen ook besloten om een document actief bij te houden waar ik duidelijk kon zien wat op wat aangesloten stond in geval dat er iets los schoot.",
+        media: ""
+    },
+    12: {
+        title: "WEEK 5: FINALE & POKÉMON THEMA",
+        text: "<strong>Afronden inviduele website layout:</strong>\nIk ben bezig geweest met het afronden van mijn layout. Vrijdag werden er nog nuttige dingen vermeld over de website en de tijdlijn waardoor ik wat aanpassingen moest doen om te voldoen aan de eisen.\nIk had een tijdlijn op een andere manier in gedachte dan bedoeld was. Ik had een externe locatie met een tijdlijn en de blog gescheiden van elkaar. Ik ben dit weekend van week 5 ook vollop bezig geweest met het afronden van de website layout zo dat dit afwas.\n\n<strong>Feedback puzzel:</strong>\nIk heb van zowel docenten als van studenten feedback gehad of heb ik dingen opgestoken van anderen hoe ze dit aangepast hebben. Zelf heb ik ook mijn eigen puzzel uit elkaar gehaald en overnieuw in elkaar gezet om een beter overzicht te creeeren. Ik heb hier ook een discord bericht naar een klasgenoot over gestuurd.\nFeedback was echt nuttig die ik gekregen heb anders was ik niet tot dit product uitgekomen.\n\n<strong>Afronden van de puzzel:</strong>\nIk ben na week 5 nog verder bezig geweest om het \"Mooi\" te maken in plaats van een schoenen doos met knoppen en displays. Ik heb hiervoor welgeteld 20+ pokemon kaarten opgeofferd en lopen schrijven en tekenen. Ik kwam toen plotseling op het idee om de code niet zomaar te geven maar met morse code wat het nog een stapje moeilijker maakte voor de gebruiker. Dit is gelukt en ben blij met de afronding die ik gedaan heb.",
+        media: ""
+    }
 };
 
-function showBlog(id) {
+function showBlog(id, element) {
     const title = document.getElementById('blog-title');
     const text = document.getElementById('blog-text');
     const media = document.getElementById('blog-media');
+    const displayCard = document.getElementById('blog-content-card');
+
+    document.querySelectorAll('.interactive-dot').forEach(dot => dot.classList.remove('active-dot'));
+
+    if (element) {
+        element.classList.add('active-dot');
+    }
+
+    if (displayCard) {
+        displayCard.classList.remove('fade-in-active');
+        void displayCard.offsetWidth;
+        displayCard.classList.add('fade-in-active');
+    }
 
     if(blogData[id]) {
         title.textContent = blogData[id].title;
-        text.textContent = blogData[id].text;
-        media.innerHTML = `<img src="${blogData[id].media}" alt="VOORTGANG AFBEELDING">`;
+        text.innerHTML = blogData[id].text;
+        media.innerHTML = "";
     }
 }
